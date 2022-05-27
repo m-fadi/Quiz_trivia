50a0f4851f98ff8583ef2e04beab192b62c93770
m-fadi <68056160+m-fadi@users.noreply.github.com>
2022-05-27 11:08:34 +0200

    Update MainMenu.js

--- src/Components/MainMenu.js
+++ src/Components/MainMenu.js
@@ -4,8 +4,12 @@ import { QuizContext } from "../utils/Context.js";
 export default function MainMenu() {
   const { gameState, setGameState } = useContext(QuizContext);
   return (
+    <div className="main-menu">
+      <h3>Quizzical</h3>
+      <p> A Quiz of 10 Questions</p>
     <button className="start-btn" onClick={() => setGameState("quiz")}>
       Start Quiz
     </button>
+    </div>
   );
 }
